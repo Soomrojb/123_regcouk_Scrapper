034cf041645a09f00e84e765ba4bee28de375f52
Soomrojb <Soomrojb@users.noreply.github.com>
2016-06-14 02:15:54 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,9 +12,15 @@ NewURL = MySite + str(today) + "/"
 #html = scraperwiki.scrape(NewURL)
 #root = lxml.html.fromstring(html)
 
-qargs = {'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/', 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'}
+qargs = {'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/'}
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
-urllib2.urlopen(NewURL, str(qargs))
+urllib2.urlopen(NewURL, headers=hdr, data=str(qargs))
 print "website loaded successfully"
 
 # root = lxml.html.fromstring(html)
