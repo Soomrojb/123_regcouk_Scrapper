6eb03ee2e1e84c23a411ddcbcbec996a8da207a1
Soomrojb <Soomrojb@users.noreply.github.com>
2016-06-14 02:02:23 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,27 +1,3 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
 
 import scraperwiki
 import lxml.html
@@ -36,7 +12,7 @@ NewURL = MySite + str(today) + "/"
 #html = scraperwiki.scrape(NewURL)
 #root = lxml.html.fromstring(html)
 
-urllib2.urlopen(NewUrl)
+urllib2.urlopen(NewURL)
 print "website loaded successfully"
 
 # root = lxml.html.fromstring(html)
