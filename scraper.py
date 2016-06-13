7359f7ed7ae9caa3ddc7728171f6a7cb7563f2cb
Soomrojb <Soomrojb@users.noreply.github.com>
2016-06-14 02:49:59 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ qargs = {'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/',
 response = requests.get(NewURL, str(qargs))
 root = lxml.html.fromstring(response.content)
 
-domains = root.xpath('//*[@id="tab-content-new-domains-tab"]/div[3]/table/tbody/tr[2]/td/a')[0]
+domains = root.xpath('//*[@id="tab-content-new-domains-tab"]/div[3]/table/tbody/tr[2]/td/a')
 
 print "website loaded successfully"
 print str(domains)
