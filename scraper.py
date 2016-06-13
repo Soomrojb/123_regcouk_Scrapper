d71d5603f9094d4eb5414922532fcaccebe27ac3
Soomrojb <Soomrojb@users.noreply.github.com>
2016-06-14 02:21:41 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@
 import scraperwiki
 import lxml.html
 import datetime
-#import urllib.request
 import urllib2
 
 today = datetime.date.today()
@@ -12,7 +11,8 @@ NewURL = MySite + str(today) + "/"
 #html = scraperwiki.scrape(NewURL)
 #root = lxml.html.fromstring(html)
 
-qargs = {'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/'}
+qargs = {'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/',
+       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11}
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -21,7 +21,7 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
        'Connection': 'keep-alive',
        'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/'}
 
-urllib2.urlopen(NewURL, str(hdr))
+urllib2.urlopen(NewURL, qargs)
 print "website loaded successfully"
 
 # root = lxml.html.fromstring(html)
