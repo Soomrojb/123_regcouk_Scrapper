535bc05f013e4a7a63228c466cf74806dac75166
Soomrojb <Soomrojb@users.noreply.github.com>
2016-06-14 01:50:05 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -26,20 +26,23 @@
 import scraperwiki
 import lxml.html
 import datetime
+import urllib
 
 today = datetime.date.today()
 MySite = 'http://www.dailychanges.com/123-reg.co.uk/';
 NewURL = MySite + str(today) + "/"
 
-html = scraperwiki.scrape(NewURL)
-root = lxml.html.fromstring(html)
+#html = scraperwiki.scrape(NewURL)
+#root = lxml.html.fromstring(html)
+
+urllib.open(NewUrl)
 
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
 
-for loop in range(1,200):
-  elem = root.cssselect("#tab-content-new-domains-tab > div:nth-child(3) > table > tbody > tr:nth-child(6) > td > a")
-  print elem
+#for loop in range(1,200):
+#  elem = root.cssselect("#tab-content-new-domains-tab > div:nth-child(3) > table > tbody > tr:nth-child(6) > td > a")
+#  print elem
 
 #print datetime.date.today().strftime("%B %d, %Y")
 
