5f5bef0a582ca3dc4a81036b5171dc73990cce1c
Soomrojb <Soomrojb@users.noreply.github.com>
2016-06-14 02:25:13 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import lxml.html
 import datetime
 import urllib2
 import requests
+import BeautifulSoup
 
 today = datetime.date.today()
 MySite = 'http://www.dailychanges.com/123-reg.co.uk/';
@@ -14,16 +15,8 @@ NewURL = MySite + str(today) + "/"
 
 qargs = {'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/',
        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
-hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive',
-       'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/'}
 
 requests.get(NewURL, str(qargs))
-#urllib2.urlopen(NewURL, str(qargs))
 print "website loaded successfully"
 
 # root = lxml.html.fromstring(html)
