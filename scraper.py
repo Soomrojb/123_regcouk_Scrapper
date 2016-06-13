6cdc72d58938916aec6e9af2f95e5a7ca6fc680a
Soomrojb <Soomrojb@users.noreply.github.com>
2016-06-14 02:22:11 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ NewURL = MySite + str(today) + "/"
 #root = lxml.html.fromstring(html)
 
 qargs = {'Referer':'http://www.dailychanges.com/123-reg.co.uk/2016-06-11/',
-       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11}
+       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
