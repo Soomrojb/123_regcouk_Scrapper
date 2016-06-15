8750802f3b46aed7391e58b0fa9998aac36b2842
Soomrojb <Soomrojb@users.noreply.github.com>
2016-06-15 15:59:04 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -25,4 +25,9 @@ for AllDomainURL in soup.findAll('a', attrs={'title':re.compile('View Whois reco
     print AllDomainURL.text
     DomainHref = AllDomainURL.get('href')
     print DomainHref
+    response1 = requests.get(DomainHref, headers=headers)
+    soup1 = BeautifulSoup(response.content)
+    print soup1
+    break
+
 
